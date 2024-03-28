38bb5b8f3c954bab9e8caeb4a788d997e7c4c12b
PatriciaAugustyn <patricia.augustyn@sorbonne-nouvelle.fr>
2024-03-28 14:26:48 +0100

    fix: ajout du programme recover_date.py

---
+++ donnees/programmes/recover_data.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+
+
+with open("larecette_url_boisson_test.txt", "r") as urls:
+    for i, line in enumerate(urls):
+        link = line.strip()
+        response = requests.get(link)
+        data = response.text
+        soup = BeautifulSoup(data, "lxml")
+        
+        narrow_contents = soup.find_all(class_="g1-content-narrow")
+        
+        for j, content in enumerate(narrow_contents):
+            with open(f"contenus/larecette_{i+1}_boisson_.html", "w", encoding="utf-8") as file:
+                file.write(str(content))
