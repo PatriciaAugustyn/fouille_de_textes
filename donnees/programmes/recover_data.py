fcb295cda305c7a432844cdd4a675c140ba8a1ff
Lise-Brisset <lise.brisset9@gmail.com>
2024-04-14 17:44:17 +0200

    feat: essaie de gestion d'erreur

--- donnees/programmes/recover_data.py
+++ donnees/programmes/recover_data.py
@@ -1,22 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
-from lxml import html
-#import os
 
 etiquettes=["entree", "plat", "dessert", "aperitif", "boisson"]
 
 for num_eti in range(len(etiquettes)):
     etiquette = etiquettes[num_eti]
 
-    with open(f"../URLs/url_{etiquette}_ent.txt", "r") as urls:
+    with open(f"../URLs/all_url_{etiquette}_ent.txt", "r") as urls:
         for i, line in enumerate(urls):
+            i+=1 # pour ne pas commencer à 0
+
             link = line.strip()
             response = requests.get(link)
-
-            #pour ceux qui utilisent lxml avec xpath (div qui n'ont pas de nom)
-            #tree = html.fromstring(response.content)
-
-            # pour ceux qui utilisent beautifusoup
             data = response.text
             soup = BeautifulSoup(data, "lxml")
 
@@ -27,43 +22,74 @@ for num_eti in range(len(etiquettes)):
             if "750g" in line:
                 site="750g"
                 narrow_contents = soup.find_all(class_="recipe-section recipe-steps") # instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
             elif "chefsimon" in line:
                 site="chefsimon"
                 narrow_contents = soup.find_all(class_="flex flex-col")
-                #narrow_contents = soup.find_all(class_="text-sm text-black-200 link_cs") # instructions
-                #content_html = tree.xpath('/html/body/div[2]/div/div[1]/div[4]/div/text')
+                if narrow_contents == []:
+                    i-=1
+                    continue
             elif "Elle-a-Table" in line:
                 site="elle"
                 narrow_contents = soup.find_all(class_="article-body") # ingrédients + instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
             elif "marmiton" in line:
                 site="marmiton"
                 narrow_contents = soup.find_all(class_="recipeV2-container") # ingrédients + instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
             elif "mesrecettesfaciles" in line:
                 site="mesrecettesfaciles"
                 narrow_contents = soup.find_all(class_="recipe-desc") # instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
             elif "regal" in line:
                 site="regal"
                 narrow_contents = soup.find_all(
                     class_="clearfix recipe-block group-description field-group-html-element"
                 ) # ingrédients + instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
             elif "larecette" in line:
                 site = "larecette"
                 narrow_contents = soup.find_all(
                     class_="g1-content-narrow g1-typography-x1 entry-content"
                 )  # ingrédients + instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
             elif "cuisineactuelle" in line:
                 site = "cuisineactuelle"
-                narrow_contents = soup.find_all(
-                    class_="recipe-content"
-                )  # ingrédients + instructions
-
+                narrow_contents = soup.find_all(class_="recipe-content")  # ingrédients + instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
+            elif "cuisineaz" in line:
+                site = "cuisineaz"
+                narrow_contents = soup.find_all(class_="borderSection instructions") # instructions
+                if narrow_contents == []:
+                    i-=1
+                    continue
             else:
                 print(line, " : site pas reconnu")
 
-            if narrow_contents:
+
+            if narrow_contents != []:
                 for j, content in enumerate(narrow_contents):
-                    with open(f"../clean-text/train/{etiquette}/{i+1}_{site}_{etiquette}.html", "w", encoding="utf-8") as file:
+                    if 0<i<10:
+                        k = "00"+str(i)
+                    elif 10<=i<=99:
+                        k = "0"+str(i)
+                    elif i>=100:
+                        k=str(i)
+                    with open(f"../clean-text/all/{etiquette}/{k}_{etiquette}_{site}.html", "w", encoding="utf-8") as file:
                         file.write(str(content))
-            #elif content_html:
-                #with open(f"../clean-text/train/{etiquette}/{i + 1}_{site}_{etiquette}.html", "w", encoding="utf-8") as file:
-                    #file.write(str(content_html))
+    print("Aspiration terminée !")
+
