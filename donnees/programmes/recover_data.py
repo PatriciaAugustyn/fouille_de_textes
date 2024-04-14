8572368658df1b195237ea94347517c6f2872801
Lise-Brisset <lise.brisset9@gmail.com>
2024-04-14 21:05:57 +0200

    feat: retour arrière gestion d'erreur ne fonctionne pas

--- donnees/programmes/recover_data.py
+++ donnees/programmes/recover_data.py
@@ -22,66 +22,48 @@ for num_eti in range(len(etiquettes)):
             if "750g" in line:
                 site="750g"
                 narrow_contents = soup.find_all(class_="recipe-section recipe-steps") # instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "chefsimon" in line:
                 site="chefsimon"
                 narrow_contents = soup.find_all(class_="flex flex-col")
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "Elle-a-Table" in line:
                 site="elle"
                 narrow_contents = soup.find_all(class_="article-body") # ingrédients + instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "marmiton" in line:
                 site="marmiton"
                 narrow_contents = soup.find_all(class_="recipeV2-container") # ingrédients + instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "mesrecettesfaciles" in line:
                 site="mesrecettesfaciles"
                 narrow_contents = soup.find_all(class_="recipe-desc") # instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "regal" in line:
                 site="regal"
                 narrow_contents = soup.find_all(
                     class_="clearfix recipe-block group-description field-group-html-element"
                 ) # ingrédients + instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "larecette" in line:
                 site = "larecette"
                 narrow_contents = soup.find_all(
-                    class_="g1-content-narrow g1-typography-x1 entry-content"
+                    class_="entry-inner"
                 )  # ingrédients + instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "cuisineactuelle" in line:
                 site = "cuisineactuelle"
                 narrow_contents = soup.find_all(class_="recipe-content")  # ingrédients + instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             elif "cuisineaz" in line:
                 site = "cuisineaz"
                 narrow_contents = soup.find_all(class_="borderSection instructions") # instructions
-                if narrow_contents == []:
-                    i-=1
-                    continue
+
             else:
                 print(line, " : site pas reconnu")
 
 
-            if narrow_contents != []:
+            if narrow_contents:
                 for j, content in enumerate(narrow_contents):
                     if 0<i<10:
                         k = "00"+str(i)
